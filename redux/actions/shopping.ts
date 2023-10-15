b0d6f2299d0d94eff23e19f569a943c12540633b
Alfredo Herrera <alfredoh@avivacredito.com>
2023-10-15 10:05:34 -0600

    add: se agrego la logica para agregar elementos al carrito

--- redux/actions/shopping.ts
+++ redux/actions/shopping.ts
@@ -1,6 +1,7 @@
+import { carShopping } from '../reducers/shopping';
 import * as t from '../typesShoppingCar';
 
-export const setDataShoppingCar = (data: []) => ({
+export const setDataShoppingCar = (data: carShopping[]) => ({
     type: t.SET_DATA_SHOPPING_CAR,
     payload: data,
 });
